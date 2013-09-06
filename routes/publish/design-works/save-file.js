5b45314c722d67263c383902188442f05e4b58ba
xiongsongsong <xiongsongsong@outlook.com>
2013-09-06 20:36:21 +0800

    使用GraphicsMagick来处理图片

--- routes/publish/design-works/save-file.js
+++ routes/publish/design-works/save-file.js
@@ -10,125 +10,145 @@
 var fs = require('fs')
 var DB = require('db')
 var path = require('path')
-var im = require('imagemagick')
 var GridStore = DB.mongodb.GridStore
 var ObjectID = DB.mongodb.ObjectID
+var gm = require('gm')
+
 
 //最大只允许上传150MB的文件
 var fileSize = 150 * 1024 * 1000
 
 exports.saveFile = function (req, res) {
 
-    res.header('Content-Type', 'text/jsoncharset=utf-8')
-
-    var files = Array.isArray(req.files.file) ? req.files.file : [req.files.file]
-
-    var tempFile = []
-
     var serverInfo = {
         err: []
     }
 
+    if (!req.files || !req.files.file) {
+        serverInfo.err.push('未接收到文件')
+        end()
+        return
+    }
+
+    var file = Array.isArray(req.files.file) ? req.files.file : [req.files.file]
+
     //虽然本方法每次“只接收”一个文件，但expressjs仍然会接收所有文件放入临时文件夹中
-    files = files.filter(function (f) {
+    file = file.filter(function (f) {
         if (f.size <= fileSize) {
-            tempFile.push(f.path)
             return true
         } else {
             //大于fileSize的文件，直接删除
             fs.unlink(f.path)
             serverInfo.err.push('不能上传大于' + fileSize + '的文件')
+            end()
             return false
         }
     })
 
-    if (require('helper').isLogin(req) === false) {
-        serverInfo.err.push('请先登陆')
-        end()
-        return
-    }
+    /*if (require('helper').isLogin(req) === false) {
+     serverInfo.err.push('请先登陆')
+     end()
+     return
+     }*/
 
-    if (files.length !== 1) {
+    if (file.length > 1) {
         serverInfo.err.push('必须且只能上传1个文件')
         end()
+        //删除所有临时文件
+        file.forEach(function (f) {
+            console.log('删除' + f.path)
+            unlink(f.path)
+        })
         return
     }
 
-    files = files[0]
+    file = file[0]
 
-    if (files.size < 1) {
+    if (file.size < 1) {
         serverInfo.err.push('不允许上传0字节文件')
         end()
         return
     }
 
-    var fileInfo = {}
     //生成一一对应的文件ID
-    files.fileId = new ObjectID()
-
-    fileInfo.name = files.name
-    fileInfo.path = path.basename(files.path)
+    file.fileId = new ObjectID()
 
     var options = {
         chunk_size: 102400,
         metadata: { }
     }
 
-    //如果上传的是图片
-    var extName = path.extname(files.name).substring(1).toLowerCase()
+    //获取文件的后缀名
+    var extName = path.extname(file.name).substring(1).toLowerCase()
     if (extName === '') extName = 'unknown'
 
-    console.log('文件名为：' + files.name, '文件扩展名是：' + extName)
+    console.log('文件名为：' + file.name, '文件扩展名是：' + extName)
 
     if (allowFile[extName]) {
         //检查是否为有效图片
-        console.log('开始对' + files.name + '文件进行合法性效验')
-        im.identify(['-format', '%wx%hx%m', files.path + '[0]'], function (err, output) {
-                if (!err) {
-                    console.log(files.name + '通过合法性效验')
-                    output = output.trim().split('x')
+        console.log('开始对' + file.name + '文件进行合法性效验')
+        gm(file.path).format(function (err, format) {
 
-                    files.width = parseInt(output[0], 10)
-                    files.height = parseInt(output[1], 10)
-                    //文件的真实格式
-                    files.format = output[2].toLowerCase()
+            //如果是允许上传的图片文件
+            if (!err && allowFile[format.toLowerCase()]) {
 
-                    console.log('有效的图片文件，原始扩展名' + extName, '真实扩展名' + files.format)
+                console.log(file.name + '是合法的图片文件，源图格式为：' + extName, '当前格式为：' + file.format)
 
-                    options.metadata.origin_name = files.name.substring(0, files.name.lastIndexOf('.') + 1) + files.format
-                    options.metadata.ext = files.format
+                //将图片的方向旋转正确，并且获取文件的尺寸
+                gm(file.path).autoOrient().size(function (err, size) {
 
-                    options.metadata.width = files.width
-                    options.metadata.owner = req.session._id
-                    options.metadata.height = files.height
+                    if (!err) {
+                        file.width = size.width
+                        file.height = size.height
+                        //文件的真实格式
+                        file.format = format
 
-                    //将meta信息发送会浏览器端
-                    serverInfo.metadata = {
-                        width: files.width,
-                        height: files.height,
-                        ext: files.format
-                    }
+                        console.log('有效的图片文件，原始扩展名' + extName, '真实扩展名' + file.format)
 
-                    //保存原始文件
-                    var fileName = files.fileId + '_w' + files.width + '_h' + files.height + '.' + files.format
-                    serverInfo._id = fileName
-                    var gs = new GridStore(DB.dbServer, fileName, fileName, "w", options)
-                    gs.writeFile(files.path, function (err) {
-                        if (!err) {
-                            end()
-                            console.log('c', Date.now())
-                        } else {
-                            serverInfo.err.push('无法保存' + files.name)
-                            end()
+                        options.metadata.origin_name = file.name.substring(0, file.name.lastIndexOf('.') + 1) + file.format
+                        options.metadata.ext = file.format
+
+                        options.metadata.width = file.width
+                        options.metadata.owner = req.session._id
+                        options.metadata.height = file.height
+
+                        //将meta信息发送会浏览器端
+                        serverInfo.metadata = {
+                            width: file.width,
+                            height: file.height,
+                            ext: file.format
                         }
-                    })
-                } else {
-                    console.log('无效的图片文件', err)
-                    serverInfo.err.push('无效的图片文件')
-                    end()
-                }
+
+                        //保存原始文件,原图的标志为：_origin
+                        var fileName = file.fileId + '_origin' + '_w' + file.width + '_h' + file.height + '.' + file.format
+                        serverInfo._id = fileName
+                        console.log('保存原始文件' + fileName)
+                        var gs = new GridStore(DB.dbServer, fileName, fileName, "w", options)
+                        gs.writeFile(file.path, function (err) {
+                            if (!err) {
+                                //开始生成并保存各种缩略图
+                                resize(file)
+                                end()
+                            } else {
+                                serverInfo.err.push('无法保存' + file.name)
+                                unlink(file.path)
+                                end()
+                            }
+                        })
+                    } else {
+                        console.log('无效的图片文件', err)
+                        serverInfo.err.push('无效的图片文件')
+                        unlink(file.path)
+                        end()
+                    }
+                })
+            } else {
+                console.log('无效的图片文件', err)
+                serverInfo.err.push('无效的图片文件')
+                unlink(file.path)
+                end()
             }
-        )
+        })
     } else {
         //其它格式，直接进行转换
         saveOriginFile()
@@ -137,29 +157,29 @@ exports.saveFile = function (req, res) {
     function end() {
         if (serverInfo.err.length < 1) {
             delete serverInfo.err
-            serverInfo.origin_name = files.name
-            serverInfo.size = files.size
+            serverInfo.origin_name = file.name
+            serverInfo.size = file.size
         }
         res.end(JSON.stringify(serverInfo, undefined, '    '))
-        unlink(tempFile)
     }
 
     function saveOriginFile() {
         console.log('非图片格式，直接进行保存，文件类型是：' + extName)
-        options.metadata.origin_name = files.name
-        var fileName = files.fileId + '.' + extName
+        options.metadata.origin_name = file.name
+        var fileName = file.fileId + '.' + extName
         serverInfo._id = fileName
         var gs = new GridStore(DB.dbServer, fileName, fileName, "w", options)
-        gs.writeFile(files.path, function (err) {
-            if (!err) {
-                console.log(files.name + '保存成功')
-            } else {
-                serverInfo.err.push(fileName + '无法保存')
-                console.log(files.name + '保存失败', err)
+        /*  gs.writeFile(files.path, function (err) {
+         if (!err) {
+         console.log(files.name + '保存成功')
+         } else {
+         serverInfo.err.push(fileName + '无法保存')
+         console.log(files.name + '保存失败', err)
+         }
+         end()
+         })*/
 
-            }
-            end()
-        })
+
     }
 }
 
@@ -170,73 +190,106 @@ var allowFile = {
     'png': 'image/png'
 }
 
+
+//各种规格
+var resizeParam = [
+    {
+        width: 20,
+        height: 20,
+        quality: 60
+    } ,
+    {
+        width: 30,
+        height: 30,
+        quality: 60
+    },
+    {
+        width: 55,
+        height: 55,
+        quality: 60
+    },
+    {
+        width: 80,
+        height: 80,
+        quality: 60
+    },
+    {
+        width: 190,
+        height: 70,
+        quality: 60
+    } ,
+    {
+        width: 230,
+        height: 175,
+        quality: 80
+    } ,
+    {
+        width: 790,
+        quality: 90
+    }
+]
+
+//对上传的图片生成不同规格的缩略图
+function resize(file) {
+
+    //过滤掉无意义的宽度（避免小图转换为大图）
+    var _resizeParam = resizeParam.filter(function (item) {
+        return file.width > item.width
+    })
+
+    function _resize() {
+        if (_resizeParam.length < 1) {
+            // unlink(file.path)
+            return
+        }
+        var cur = _resizeParam.shift()
+
+        //生成缩略图并存入库中
+        var fileName = file.fileId + '_w' + cur.width + '_h' + cur.height + '.' + file.format
+
+        //生成图片后的路径
+        var dstSrc = path.join(path.dirname(file.path), fileName).toString()
+
+        //生成缩略图，并且为60%的质量
+        gm(file.path).resize(cur.width, (cur.height ? cur.height : null)).noProfile().quality(cur.quality).write(dstSrc, function (err) {
+            if (!err) {
+                var gs = new GridStore(DB.dbServer, fileName, fileName, "w", {
+                    "chunk_size": 10240
+                })
+                gs.writeFile(dstSrc, function (err) {
+                    if (err) console.log(err)
+                    //  unlink(dstSrc)
+                    _resize()
+                })
+            } else {
+                console.log(err)
+                //  unlink(dstSrc)
+                _resize()
+            }
+        })
+    }
+
+    _resize()
+}
+
+
 /*
+ //清理各种临时文件
  该方法并不能完全解决问题
  此处应该使用定时程序，来做处理
  */
 
-function unlink(list) {
-    if (list.length < 1) return
-    var cur = list.shift()
-    fs.unlink(cur, function (err) {
+function unlink(file) {
+    if (!file) return
+    fs.unlink(file, function (err) {
         if (!err) {
-            console.log(cur + '\t already unlink')
+            console.log(file + '\t already unlink')
         } else {
-            console.log('unlink fail', err)
+            console.log('unlink fail', err, file)
         }
-        if (list.length > 0) unlink(list)
     })
 }
 
 var app = require('app')
 
 app.post('/publish/design-works/save-file', exports.saveFile)
-
-
-
-//按比例缩放，只针对宽度
-var resizeParam = [
-
-];
-//对上传的图片生成不同规格的缩略图
-function resize(cur, fileId, cb) {
-    var _resizeParam = resizeParam.filter(function (item) {
-        return cur.width > item;
-    });
-
-    function _resize() {
-        if (_resizeParam.length < 1) {
-            cb();
-            return;
-        }
-        var curSize = _resizeParam.shift();
-
-        //生成缩略图并存入库中
-        var dstSrc = cur.path + '_' + curSize;
-        im.resize({
-            srcPath: cur.path,
-            dstPath: dstSrc,
-            width: curSize
-        }, function (err) {
-            if (!err) {
-
-                console.log(curSize + '规格缩略图生成完毕');
-                tempFile.push(dstSrc);
-
-                var gs = new GridStore(DB.dbServer, fileId + '_' + curSize, "w", {
-                    "chunk_size": 10240
-                });
-                gs.writeFile(dstSrc, function (err) {
-                    if (err) console.log(err);
-                    _resize();
-                });
-            } else {
-                console.log(err);
-                _resize();
-            }
-
-        });
-    }
-
-    _resize();
-}
