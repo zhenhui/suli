c174b544a25f01fba231588eb7785c6ab78523dc
xiongsongsong <xiongsongsong@outlook.com>
2013-09-26 14:05:06 +0800

    增加Etag并启用304支持

--- routes/read/index.js
+++ routes/read/index.js
@@ -9,22 +9,23 @@ var app = require('app')
 var DB = require('db')
 
 app.get(/\/read\/(.+)/i, function (req, res) {
-
-    var id = req.params && req.params[0]
-
-    var gs = new DB.mongodb.GridStore(DB.dbServer, req.params[0], "r");
-    res.header('Cache-Control', 'max-age=315360000');
-    res.header('Expires', ' Tue, 10 Jan 2023 02:19:00 GMT');
-    res.header('Last-Modified', ' Tue, 10 Jan 2000 02:19:00 GMT');
+    var gs = new DB.mongodb.GridStore(DB.dbServer, req.params[0], "r")
+    res.header('Cache-Control', 'max-age=315360000')
+    res.header('Expires', ' Tue, 10 Jan 2023 02:19:00 GMT')
+    res.header('Last-Modified', ' Tue, 10 Jan 2000 02:19:00 GMT')
     gs.open(function (err, gs) {
         if (!err) {
-            gs.read(gs.length, function (err, data) {
-                res.end(data);
-            });
+            if (req.headers['if-none-match'] !== gs.internalMd5) {
+                res.header('ETag', gs.internalMd5)
+                gs.read(gs.length, function (err, data) {
+                    res.end(data)
+                })
+            } else {
+                res.status(304)
+                res.end()
+            }
         } else {
-            res.end('read error');
+            res.end()
         }
-
-    });
-
+    })
 })
