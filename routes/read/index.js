f58ac4862b8807593f5763051e5b110cddde0698
xiongsongsong <xiongsongsong@outlook.com>
2013-09-27 09:37:10 +0800

    启用缓存

--- routes/read/index.js
+++ routes/read/index.js
@@ -9,23 +9,22 @@ var app = require('app')
 var DB = require('db')
 
 app.get(/\/read\/(.+)/i, function (req, res) {
-    var gs = new DB.mongodb.GridStore(DB.dbServer, req.params[0], "r")
     res.header('Cache-Control', 'max-age=315360000')
     res.header('Expires', ' Tue, 10 Jan 2023 02:19:00 GMT')
     res.header('Last-Modified', ' Tue, 10 Jan 2000 02:19:00 GMT')
-    gs.open(function (err, gs) {
-        if (!err) {
-            if (req.headers['if-none-match'] !== gs.internalMd5) {
-                res.header('ETag', gs.internalMd5)
+    if (req.headers['if-modified-since']) {
+        res.status(304)
+        res.end()
+    } else {
+        var gs = new DB.mongodb.GridStore(DB.dbServer, req.params[0], "r")
+        gs.open(function (err, gs) {
+            if (!err) {
                 gs.read(gs.length, function (err, data) {
                     res.end(data)
                 })
             } else {
-                res.status(304)
                 res.end()
             }
-        } else {
-            res.end()
-        }
-    })
+        })
+    }
 })
