80c0fbab0b2cebf35be0733c5ca8a321c5d70461
松松 <xiongsongsong@outlook.com>
2013-11-13 09:45:25 +0800

    修复个人空间作品和文章无法自动高亮的问题

--- assets/s/u/init.js
+++ assets/s/u/init.js
@@ -10,25 +10,26 @@ define(function (require, exports, module) {
     KISSY.use('event', function (S, Event) {
         function hashChange() {
             $('#J-navigation-trigger a').removeClass('active')
+            var hash = location.hash.substring(1)
             switch (location.hash) {
                 case "#design-works":
                     require.async('./design-works', function (obj) {
                         obj.init()
+                        $('#J-navigation-trigger a.' + hash).addClass('active')
                     })
-                    $('#J-navigation-trigger a.design-works').addClass('active')
                     break;
                 case "#article":
                     require.async('./article', function (obj) {
                         obj.init()
+                        $('#J-navigation-trigger a.' + hash).addClass('active')
                     })
-                    $('#J-navigation-trigger a.article').addClass('active')
                     break;
                 default:
                     if (location.hash !== '#design-works' && location.hash !== '#article') {
                         require.async('./design-works', function (obj) {
                             obj.init()
+                            $('#J-navigation-trigger a.design-works').addClass('active')
                         })
-                        $('#J-navigation-trigger a.article').addClass('active')
                     }
                     break;
             }
