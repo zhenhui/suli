89662b8bab8f9ce087e4e7e84c1a16eed2b18f41
松松 <xiongsongsong@outlook.com>
2013-11-27 21:06:22 +0800

    左侧浮动菜单制作完毕

--- assets/s/global/weixin-weibo-float-menu/init.js
+++ assets/s/global/weixin-weibo-float-menu/init.js
@@ -9,5 +9,25 @@ define(function (require, exports, module) {
     wrapper.id = 'weixin-weibo-float-menu-wrapper'
     wrapper.innerHTML = tpl
     document.body.appendChild(wrapper)
+
+    var $float = $('#weixin-weibo-float-menu-wrapper')
+
+    $float.on('click', '.J-top', function () {
+        $(document.body).animate({scrollTop: 0}, 300, 'swing', function () {
+        });
+    })
+    $float.on('mouseenter mouseleave', '.J-weixin-trigger', function (ev) {
+        if (ev.type === 'mouseenter') {
+            $float.find('.weixin-tips').stop().fadeIn(100)
+        } else {
+            $float.find('.weixin-tips').stop().fadeOut(100)
+        }
+    })
+
+    $float.on('click', '.J-weixin-trigger', function () {
+        $float.find('.weixin-tips').stop().fadeToggle(100)
+    })
+
+
 })
 
