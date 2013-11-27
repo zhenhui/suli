3a0f16004edc0c196e4dd92c15babb2e87617126
松松 <xiongsongsong@outlook.com>
2013-11-27 21:47:45 +0800

    增加底部反馈的文案

--- assets/s/global/init.js
+++ assets/s/global/init.js
@@ -12,6 +12,13 @@ define(function (require, exports, module) {
         require('./login/login')
         require('./publish-type/init')
         require('./weixin-weibo-float-menu/init')
+
+        var href = location.href
+
+        $('<div id="feedback"><h4>反馈</h4><a href="mailto:hangzhenhui@beyondsoft.com?subject='+encodeURIComponent('页面交互、功能性建议')+'&amp;cc=xiongsongsong@beyondsoft.com&amp;body=url:' + href + '">1：页面、优化、功能性建议</a>' +
+            '<a href="mailto:xiongsongsong@beyondsoft.com?subject='+encodeURIComponent('页面报错，点击不正常，无法使用')+'&amp;cc=hangzhenhui@beyondsoft.com&amp;body=url:' + href + '">2：页面报错，点击不正常，无法使用</a></div>')
+            .appendTo($('#footer-container'))
+
     })
 })
 
