58dcabdbf3cc5c77a8e8f21ffe9887426d6ed4d5
松松 <xiongsongsong@outlook.com>
2013-11-30 15:58:01 +0800

    modify console

--- assets/s/admin/user/manager.js
+++ assets/s/admin/user/manager.js
@@ -86,7 +86,7 @@ define(function (require, exports, module) {
                     $('#restartPM2idResult').val('重启貌似没有成功，请等5秒后刷新页面，或赶紧联系管理员')
                 }
             }).error(function () {
-                $('#restartPM2idResult').val('连接已断开，说明，' + id + '进程正在重启中，一般耗时在5秒内 ')
+                $('#restartPM2idResult').val('连接已断开，说明：' + id + '进程正在重启中，一般耗时在5秒内')
             })
     })
 })
