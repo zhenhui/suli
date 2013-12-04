0531028b71f82c249656ca135cad1bd77e81a29d
song <xiongsongsong@outlook.com>
2013-12-04 23:37:03 +0800

    add memcach

--- routes/cms/go.js
+++ routes/cms/go.js
@@ -7,17 +7,18 @@ var app = require('app')
 var helper = require('./helper')
 var template = require('template')
 
+//test
+var Memcached = require('memcached')
+
+var memServer = new Memcached('127.0.0.1:11211');
+
+
 exports.mimeTypeMap = {
     js: 'application/javascript;charset=utf-8',
     jsonp: 'application/javascript;charset=utf-8',
     css: 'text/css;charset=utf-8',
     default: 'text/html;charset=utf-8'
 }
-
-exports.cache = Object.create(null)
-
-//缓存在内存中存活的阈值
-exports.ExpiresTs = 3600000
 
 app.get(/^\/go\/(.+)/, function (req, res) {
     var page_url = req.params && req.params[0] ? req.params[0] : false
@@ -35,7 +36,7 @@ app.get(/^\/go\/(.+)/, function (req, res) {
         res.header('content-type', mimeType)
         try {
             if (str.indexOf(helper.isDynmaic) > -1) {
-                str = template.render(str, {})
+                str = template.render(str.replace(helper.isDynmaic, ''), {})
             }
             if (req.query.callback) {
                 res.end(req.query.callback + "(" + str + ");")
@@ -43,7 +44,7 @@ app.get(/^\/go\/(.+)/, function (req, res) {
                 res.end(str)
             }
         } catch (e) {
-            res.end('500 error')
+            res.end('500 error' + e.toString())
         }
     })
 })
@@ -55,47 +56,39 @@ exports.readPage = function (page_url, callback) {
         return
     }
 
-    if (exports.cache[page_url]) {
-        console.log(page_url + '命中缓存')
-        callback(exports.cache[page_url].source)
-        //更新时间戳
-        exports.cache[page_url].ts = Date.now()
-    } else {
-        console.log(page_url + '开始从磁盘读取')
-        var filePath = path.join(helper.staticBaseDir, page_url + '.jstpl')
-        fs.readFile(filePath, function (err, buffer) {
-            if (err) {
-                console.log(page_url + '读取失败')
-                callback('Not Found', 404)
-                return
-            }
-            console.log(page_url + '读取成功')
-            callback(buffer.toString())
-            exports.cache[page_url] = Object.create(null)
-            exports.cache[page_url].source = buffer.toString()
-            exports.cache[page_url].ts = Date.now()
-            //将内存中超过maxExpiresTs阈值的缓存清除掉
-            exports.clearMemory()
-        })
-    }
-}
 
-//通过删除缓存，达到更新的目的
-exports.update = function (page_url) {
-    console.log('开始更新缓存' + page_url)
-    if (exports.cache[page_url]) {
-        delete exports.cache[page_url]
-        console.log('更新了' + page_url + '的缓存')
-    }
-}
+    memServer.get(page_url, function (err, data) {
 
-//将访问量少的页面，从内存中移除
-exports.clearMemory = function () {
-    console.log('开始清理缓存')
-    Object.keys(exports.cache).forEach(function (item) {
-        if (Date.now() - exports.cache[item].ts > exports.ExpiresTs) {
-            console.log(item + '被清理')
-            delete exports.cache[item]
+
+        if (!err && data) {
+            console.log(page_url + '命中缓存')
+            callback(data)
+        } else {
+            console.log(page_url + '开始从磁盘读取')
+            var filePath = path.join(helper.staticBaseDir, page_url + '.jstpl')
+            fs.readFile(filePath, function (err, buffer) {
+                if (err) {
+                    console.log(page_url + '读取失败')
+                    callback('Not Found', 404)
+                    return
+                }
+                console.log(page_url + '读取成功')
+                callback(buffer.toString())
+                memServer.set(page_url, buffer.toString(), 3600, function (err) {
+                    if (err) console.log('Fail:Not set:' + page_url + ' to memcached')
+                });
+            })
         }
-    })
+
+    });
+}
+
+exports.update = function (page_url) {
+    memServer.del(page_url, function (err) {
+        if (err) {
+            console.log('Fail:Can not delete:' + page_url + ' at memcached')
+        } else {
+            console.log('Success:delete:' + page_url + ' at memcached')
+        }
+    });
 }
