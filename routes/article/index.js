5a6885888c48c2b67db5277b06f089f47c61fa67
松松 <xiongsongsong@outlook.com>
2013-12-04 10:51:08 +0800

    fix    bug：未能增加csrf

--- routes/article/index.js
+++ routes/article/index.js
@@ -77,7 +77,7 @@ app.get('/personal/article/list', function (req, res) {
 
 
 //文章详情页
-app.get(/^\/article\/(.+)/, function (req, res) {
+app.get(/^\/article\/(.+)/, helper.csrf, function (req, res) {
     var title = req.params[0]
     var article = new db.mongodb.Collection(db.Client, 'article')
     article.findOne({title: title, status: {$gte: 1}}, function (err, result) {
