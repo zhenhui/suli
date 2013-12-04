094ff263381d51c0aa6ce6cf637639cb4e8b7347
松松 <xiongsongsong@outlook.com>
2013-12-04 18:41:21 +0800

    增加一个获取token的接口

---
+++ routes/token/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by 松松 on 13-12-4.
+ */
+
+var app = require('app')
+
+app.get('/_csrf_token', function (req, res) {
+    res.header('Cache-control', 'no-cache; must-revalidate')
+    res.header('Expires', '-1');
+    res.header('Last-Modified', ' Tue, 10 Jan 2000 02:19:00 GMT');
+    res.header('Pragma', 'no-cache');
+    res.header('content-type', 'application/javascript')
+    if (req.query.callback) {
+        res.end(req.query.callback + '("' + req.csrfToken() + '")')
+    } else {
+        res.end('window._csrf_token_="' + req.csrfToken() + '"')
+    }
+})
