e3a8b8d4c69b6948cdfd1987a40f3c5a69d99865
song <xiongsongsong@outlook.com>
2013-12-09 22:19:17 +0800

    can send email!

--- routes/register/index.js
+++ routes/register/index.js
@@ -14,5 +14,38 @@ app.get('/register', function (req, res) {
 app.post('/register', function (req, res) {
 
     res.json(req.body)
+    sendEmail(JSON.stringify(req.body,undefined,'\t'))
 
 })
+
+
+var nodemailer = require("nodemailer");
+
+function sendEmail(str){
+
+    var transport = nodemailer.createTransport("SMTP", {
+        host: "smtp.126.com",
+        secureConnection: true, // use SSL
+        port: 465, // port for secure SMTP
+        auth: {
+            user: "xiong_song@126.com",
+            pass: "*****"
+        }
+    });
+
+    transport.sendMail({
+        from : "xiong_song@126.com",
+        to : "xiongsongsong@beyondsoft.com",
+        subject: "主题？",
+        generateTextFromHTML : true,
+        html : '<h1>asdf<b style="background: red">asdf</b>asdfasdfsadf</h1>'+str
+    }, function(error, response){
+        if(error){
+            console.log(error);
+        }else{
+            console.log("Message sent: " + response.message);
+        }
+        transport.close();
+    });
+
+}
