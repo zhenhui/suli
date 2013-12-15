bb709c08399016c53dcc233d7c78a1fca21a3cd3
song <xiongsongsong@outlook.com>
2013-12-15 15:17:34 +0800

    返回上传者姓名

--- routes/design-works/list.js
+++ routes/design-works/list.js
@@ -113,7 +113,7 @@ app.get('/design-works/latest/list', function (req, res) {
         result.page = page
 
         design.find(defaultFindParam, {
-            _id: 1, title: 1, content: 1, thumbnails_id: 1, file_id: 1, owner_id: 1, index: 1}).sort({ts: -1}).
+            _id: 1, title: 1, content: 1, thumbnails_id: 1, file_id: 1, owner_user: 1, owner_id: 1, index: 1}).sort({ts: -1}).
             skip((page - 1) * pageCount).limit(pageCount).toArray(function (err, docs) {
                 result.data = docs
                 res.jsonp(result)
