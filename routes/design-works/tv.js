fef0943240cebec385e18e3e70fe609f14828b8b
熊松松 <xiongsongsong@outlook.com>
2014-04-28 15:31:05 +0800

    Update tv.js

--- routes/design-works/tv.js
+++ routes/design-works/tv.js
@@ -4,26 +4,26 @@ var app = require('app')
 
 //删除作品
 app.get('/design-works/up-tv', function (req, res) {
-  
-  helper.getGroup(req, function (group) {
-      
-      if (Array.isArray(group) === false) {
-            res.end('')
-            return
-        }
-        
-        if (group.indexOf('前台电视作品控制') <0) {
+
+    helper.getGroup(req, function (group) {
+
+        if (Array.isArray(group) === false) {
             res.end('')
             return
         }
 
-      
+        if (group.indexOf('前台电视作品控制') < 0) {
+            res.end('')
+            return
+        }
+
+
         var result = {err: []}
-    
+
         var design = new DB.mongodb.Collection(DB.Client, 'design-works')
-    
+
         //上电视
-        design.update({_id: id, owner_id: req.session._id}, {$set: {tv : req.query.tv === 'true' ? true : false }}, {}, function (err, num) {
+        design.update({_id: id, owner_id: req.session._id}, {$set: {tv: req.query.tv === 'true' ? true : false }}, {}, function (err, num) {
             if (!err && num > 0) {
                 result.status = 1
                 delete result.err
@@ -32,8 +32,8 @@ app.get('/design-works/up-tv', function (req, res) {
                 result.err.push('无法上电视')
             }
             res.jsonp(result)
-    
+
         })
-  })
-  
+    })
+
 })
